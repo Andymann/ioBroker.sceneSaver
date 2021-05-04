ddc7741513a8cb2a8b10304b2ea7a44bc97d4838
Andy Fischer <info@andyland.info>
2021-05-04 09:27:27 +0200

    add script counter to admin interface

--- main.js
+++ main.js
@@ -7,9 +7,12 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
+const { formatWithOptions } = require('node:util');
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
+let parentThis;
+
 
 class Scenesaver extends utils.Adapter {
 
@@ -26,6 +29,8 @@ class Scenesaver extends utils.Adapter {
 		// this.on('objectChange', this.onObjectChange.bind(this));
 		// this.on('message', this.onMessage.bind(this));
 		this.on('unload', this.onUnload.bind(this));
+
+		parentThis = this;
 	}
 
 	/**
@@ -38,12 +43,14 @@ class Scenesaver extends utils.Adapter {
 		// this.config:
 		this.log.info('config option1: ' + this.config.option1);
 		this.log.info('config option2: ' + this.config.option2);
+		this.log.info('config sceneCount: ' + this.config.sceneCount);
 
 		/*
 		For every state in the system there has to be also an object of type state
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
+		/*
 		await this.setObjectNotExistsAsync('testVariable', {
 			type: 'state',
 			common: {
@@ -55,34 +62,38 @@ class Scenesaver extends utils.Adapter {
 			},
 			native: {},
 		});
+		*/
 
 		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates('testVariable');
+		//this.subscribeStates('testVariable');
 		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
 		// this.subscribeStates('lights.*');
 		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
 		// this.subscribeStates('*');
+
 
 		/*
 			setState examples
 			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
 		*/
 		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync('testVariable', true);
+		//await this.setStateAsync('testVariable', true);
 
 		// same thing, but the value is flagged "ack"
 		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync('testVariable', { val: true, ack: true });
+		//await this.setStateAsync('testVariable', { val: true, ack: true });
 
 		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+		//await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
 
 		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync('admin', 'iobroker');
-		this.log.info('check user admin pw iobroker: ' + result);
+		//let result = await this.checkPasswordAsync('admin', 'iobroker');
+		//this.log.info('check user admin pw iobroker: ' + result);
 
-		result = await this.checkGroupAsync('admin', 'admin');
-		this.log.info('check group user admin group admin: ' + result);
+		//result = await this.checkGroupAsync('admin', 'admin');
+		//this.log.info('check group user admin group admin: ' + result);
+
+		this.createStateAsync();
 	}
 
 	/**
@@ -152,6 +163,16 @@ class Scenesaver extends utils.Adapter {
 	// 		}
 	// 	}
 	// }
+
+
+	async createStates() {
+		this.log.info('createStates()');
+		let iScenecount = parseInt(this.config.sceneCount);
+		for (let i = 0; i < iScenecount; i++) {
+			this.log.info('erzeuge() ' + i);
+		}
+
+	}
 
 }
 
